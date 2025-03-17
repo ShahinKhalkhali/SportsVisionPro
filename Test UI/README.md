6089e3ab93662286710796616933930dde8d79e5
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2025-03-17 13:14:21 -0400

    Revised styling of the webpage

--- Test UI/README.md
+++
@@ -1,1 +0,0 @@
-Open index.html to display a sample of what the webpage will look like
--- embeddedApp/uwb.h
+++ embeddedApp/uwb.h
@@ -1,4 +1,5 @@
 #pragma once
 
 // TODO:  CREATE A WORKING HEADER FILE WITH MAIN embeddedApp.ino
-//        USING DW1000Ng 
+//        USING DW1000Ng
+
